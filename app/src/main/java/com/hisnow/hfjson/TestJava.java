1be3676968da33859336ad3d7c10dccd598a3c89
zhangmin <zhangmin@gmail.com>
2017-09-27 15:21:27 +0800

    test

---
+++ app/src/main/java/com/hisnow/hfjson/TestJava.java
@@ -0,0 +1,11 @@
+package com.hisnow.hfjson;
+
+/**
+ * Created by Administrator on 2017/9/27 0027.
+ */
+
+public class TestJava {
+    public static String returnStr(String str) {
+        return str;
+    }
+}
