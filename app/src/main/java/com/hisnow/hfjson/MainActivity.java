d28d9fa4ef2f17de8755dbf8a5d4fcdac22828a0
zhangmin <zhangmin@gmail.com>
2017-09-27 09:30:16 +0800

    test

--- app/src/main/java/com/hisnow/hfjson/MainActivity.java
+++ app/src/main/java/com/hisnow/hfjson/MainActivity.java
@@ -7,6 +7,6 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);//
+        super.onCreate(savedInstanceState);///
     }
 }
